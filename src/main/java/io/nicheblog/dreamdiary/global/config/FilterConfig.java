617a30aafc549f1689aa9d477bdb94a9228c7af0
nichefish <nichefish@gmail.com>
2026-03-21 15:28:47 +0900

    - jandi, mqtt, snmp -> infrastructure 밑으로 이동

--- src/main/java/io/nicheblog/dreamdiary/global/config/FilterConfig.java
+++ src/main/java/io/nicheblog/dreamdiary/global/config/FilterConfig.java
@@ -1,6 +1,6 @@
 package io.nicheblog.dreamdiary.global.config;
 
-import io.nicheblog.dreamdiary.global.filter.TraceFilter;
+import io.nicheblog.dreamdiary.infrastructure.log.actvty.filter.TraceFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/io/nicheblog/dreamdiary/infrastructure/notify/jandi/JandiTopic.java
@@ -0,0 +1,18 @@
+package io.nicheblog.dreamdiary.infrastructure.notify.jandi;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * JandiTopic
+ * <pre>
+ *  잔디 토픽 Enum
+ * </pre>
+ *
+ * @author nichefish
+ */
+@RequiredArgsConstructor
+public enum JandiTopic {
+    NOTICE,
+    SCHDUL,
+    TEST;
+}
