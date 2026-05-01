d7238db166b1b6b99001d11ffc52c274fbd87d2a
nichefish <nichefish@gmail.com>
2026-05-02 02:27:04 +0900

    - 마크다운 ------ → --- 로 수정

--- static/js/view/feature/journal/interpretation/journal_interpretation_module.ts
+++ static/js/view/feature/journal/interpretation/journal_interpretation_module.ts
@@ -295,7 +295,7 @@ dF.JournalInterpretation = (function(): dfModule {
                 const resultCn: string = rsltObj.content;
                 // 문단/줄바꿈을 먼저 텍스트로 치환
                 const replacedCn: string = resultCn
-                    .replace(/<\s*hr\b[^>]*\/?>/gi, "\n------\n")
+                    .replace(/<\s*hr\b[^>]*\/?>/gi, "\n---\n")
                     .replace(/<\s*br\s*\/?>/gi, "\n")
                     .replace(/<\s*\/?p[^>]*>/gi, "\n");
                 const div: HTMLDivElement = document.createElement("div");
